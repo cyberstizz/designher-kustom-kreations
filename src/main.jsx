e59a5229238a3e72fa1f4d8c2be17ee43b2f561a
Charles Lamb <charles.lamb.dev@gmail.com>
2026-09-15 13:46:53 -0400

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/global.css';
+import './styles/order-book.css';
 
 /* Note: no <React.StrictMode> on purpose.
    The ported page scripts in src/pages/scripts/ attach DOM event listeners
